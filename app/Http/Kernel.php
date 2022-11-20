2be1f0a477c386c21c44d18fe296f796e84e2523
ibanrama <syaiban95@gmail.com>
2022-11-20 18:48:07 +0700

    crate middleware penjual

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'penjual' => \App\Http\Middleware\Penjualan::class
     ];
 }
