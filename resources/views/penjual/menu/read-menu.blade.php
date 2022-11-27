9df569057e01a3a534e78461507bf62c9366a602
ibanrama <syaiban95@gmail.com>
2022-11-28 00:18:21 +0700

    fix datatables

--- resources/views/penjual/menu/read-menu.blade.php
+++ resources/views/penjual/menu/read-menu.blade.php
@@ -19,9 +19,6 @@
                     <div class="row">
                         <div class="col-lg-12">
                             <div class="card mb-4">
-                                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-primary">DataTables with Hover</h6>
-                                </div>
                                 <div class="table-responsive p-3">
                                     <table class="table align-items-center table-flush table-hover" id="dataTableHover">
                                         <thead class="thead-light">
